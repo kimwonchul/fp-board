7eb491ee6a384f6234b206549f4e220e0915b49e
kim.wonchul <kim.wonchul@cnrres.com>
2023-11-06 15:19:31 +0900

    #6-스프링부트프로젝트시작 * 스프랭앱...

---
+++ fp-board/src/main/java/com/fp/fpboard/FpBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fp.fpboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpBoardApplication.class, args);
+	}
+
+}
